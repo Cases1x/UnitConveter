86323fd0a5089f64a53fab33fd81f43e797171f5
Cases1x <francisvargas121302@gmail.com>
2023-05-30 13:57:04 +0800

    Hardcoding each conversions

---
+++ Converter.cs
@@ -0,0 +1,11 @@
+namespace UnitConverter
+{
+    public class Converter
+    {
+        public double FootToInch(double Foot)
+        {
+            double Inch = Foot * 12;
+            return Inch;
+        }
+    }
+}
