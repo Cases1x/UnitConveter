2a26f066ab83d0a34feb62b7f142baff72e25432
Cases1x <francisvargas121302@gmail.com>
2023-05-31 15:35:15 +0800

    tinuloy ko rin yung switch statments

--- Converter.cs
+++ Converter.cs
@@ -2,6 +2,202 @@ namespace UnitConverter
 {
     public class Converter
     {
+        public double convert(double value, int source, int target)
+        {
+            switch (source)
+            {
+                case 1:
+                    switch (target)
+                    {
+                        case 1:
+                            return value;
+                            
+                        case 2:
+                            return FootToInch(value);
+                        case 3:
+                            return FootToYard(value);
+                        case 4:
+                            return FootToMeter(value);
+                        case 5:
+                            return FootToMile(value);
+                        case 6:
+                            return FootToKilometer(value);
+                        case 7:
+                            return FootToCentimeter(value);
+                        case 8:
+                            return FootToCentimeter(value);
+                        default:
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (target)
+                    {
+                        case 1:
+                            return InchToFoot(value);
+                        case 2:
+                            return value;
+                        case 3:
+                            return InchToYard(value);
+                        case 4:
+                            return InchToMeter(value);
+                        case 5:
+                            return InchToMile(value);
+                        case 6:
+                            return InchToKilometer(value);
+                        case 7:
+                            return InchToCentimeter(value);
+                        case 8:
+                            return InchToCentimeter(value);
+                        default:
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch (target)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 4:
+                    switch (target)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 5:
+                    switch (target)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 6:
+                    switch (target)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 7:
+                    switch (target)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 8:
+                    switch (target)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return 0;
+        }
+
+
         public double FootToInch(double foot)
         {
             return foot * 12;
@@ -35,37 +231,38 @@ namespace UnitConverter
             return foot * 304.8;
         }
 
-        public double InchtoFoot(double inch)
+        public double InchToFoot(double inch)
         {
             return inch / 12;
         }
-        public double InchtoYard(double inch)
+        public double InchToYard(double inch)
         {
             return inch / 36;
         }
-        public double InchtoMeter(double inch)
+        public double InchToMeter(double inch)
         {
             return inch / 39.37;
         }
 
-        public double InchtoMile(double inch)
+        public double InchToMile(double inch)
         {
             return inch / 63360;
         }
-        public double InchtoKilometer(double inch)
+        public double InchToKilometer(double inch)
         {
             return inch / 39370;
         }
 
-        public double InchtoCentimeter(double inch)
+        public double InchToCentimeter(double inch)
         {
             return inch * 2.54;
         }
 
-         public double InchtoMilimeter(double inch)
+         public double InchToMilimeter(double inch)
         {
             return inch * 25.4;
         }
+        
     }
 }
 
@@ -73,13 +270,11 @@ namespace UnitConverter
 
 
 
-/*
-Foot
-Inch
-Yard
-Meter
-Mile
-Kilometer
-Centimeter
-Milimeter
-*/
+
+
+
+
+
+
+
+
--- Program.cs
+++ Program.cs
@@ -8,14 +8,67 @@
             // then saka mag iinput ng i cconvert,
             // then mag cconvert unli until you decide to change whether kung ano cconvert mo, and kung san mo i cconvert. 
             // I can you parameters dito, parang ang nasa parameters whether kung ano yung unang unit and kung saan i cconvert
-            PrintUnit();    
-            Console.WriteLine("Choose a unit to convert from: ");
-            var sourceunit = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Choose a unit to convert to: ");
-            var targetunit = Convert.ToInt32(Console.ReadLine());
             
+            Converter converter = new Converter();
+            
+            PrintLengths();    
+            Console.WriteLine("Choose a unit to convert from: ");
+            var source = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose a unit to convert to: ");
+            var target = Convert.ToInt32(Console.ReadLine());
+            var sourceunit = ReturnUnit(source);
+            var targetunit = ReturnUnit(target);
+            Console.WriteLine($"Converting from {sourceunit} to {targetunit}");
+            Console.WriteLine("Enter the value you want to convert: ");
+            var value = Convert.ToInt32(Console.ReadLine());
 
+            double converted = converter.convert(value, source, target);
 
+            Console.WriteLine($"{value} {sourceunit} is equal to {converted} {targetunit}");
         }
+
+        private static string ReturnUnit(int unit)
+        {
+            
+            switch (unit)
+            {
+                case 1:
+                    return "Foot";
+                case 2:
+                    return "Inch";
+                case 3:
+                    return "Yard";
+                case 4:
+                    return "Meter";
+                case 5:
+                    return "Mile";
+                case 6:
+                    return "Kilometer";
+                case 7:
+                    return "Centimeter";
+                case 8:
+                    return "Millimeter";
+                default:
+                    return "";
+             
+            }
+            
+        }
+
+        private static void PrintLengths()
+        {
+            Console.WriteLine("1. Foot");
+            Console.WriteLine("2. Inch");
+            Console.WriteLine("3. Yard");
+            Console.WriteLine("4. Meter");
+            Console.WriteLine("5. Mile");
+            Console.WriteLine("6. Kilometer");
+            Console.WriteLine("7. Centimeter");
+            Console.WriteLine("8. Milimeter");
+        }
+
+
+        
+
     }
 }
