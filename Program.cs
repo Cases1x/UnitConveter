50f91227bed9e546715d5e121b8d188cd59e85c0
Cases1x <francisvargas121302@gmail.com>
2023-05-31 14:35:12 +0800

    disregard this commit, switching purposes only

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace UnitConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mamimili muna ng from san mag cconvert and san i cconvert
+            // then saka mag iinput ng i cconvert,
+            // then mag cconvert unli until you decide to change whether kung ano cconvert mo, and kung san mo i cconvert. 
+            // I can you parameters dito, parang ang nasa parameters whether kung ano yung unang unit and kung saan i cconvert
+            PrintUnit();    
+            Console.WriteLine("Choose a unit to convert from: ");
+            var sourceunit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose a unit to convert to: ");
+            var targetunit = Convert.ToInt32(Console.ReadLine());
+            
+
+
+        }
+    }
+}
